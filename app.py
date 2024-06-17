53075b941aca0c8e376fa8354aad18adf4978f62
Yoann Betton <yoann.betton@hotmail.fr>
2024-06-17 16:16:04 +0200

    change downloaded file name

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from lxml import etree
 
 app = Flask(__name__)
 
-# Dictionnaires pour les codes de couleur et les préfixes des noms
 color_codes = {
     "Red": 0,
     "Yellow": 1,
@@ -31,7 +30,7 @@ def get_color_code(name):
     for prefix, color in name_prefixes.items():
         if name.startswith(prefix):
             return color_codes[color]
-    return color_codes["Black"]  # Default color if no prefix matches
+    return color_codes["Black"]
 
 def get_gp39symbol(name):
     if name.startswith("D"):
@@ -43,14 +42,12 @@ def convert_gpx(input_file):
     tree = etree.parse(input_file, parser)
     root = tree.getroot()
     
-    # Remove default namespace
     for elem in root.getiterator():
         if not hasattr(elem.tag, 'find'): continue
         i = elem.tag.find('}')
         if i >= 0:
             elem.tag = elem.tag[i+1:]
     
-    # Create a new root for the output GPX
     nsmap = {
         None: 'http://www.topografix.com/GPX/1/1',
         'xsi': 'http://www.w3.org/2001/XMLSchema-instance',
@@ -58,7 +55,6 @@ def convert_gpx(input_file):
     }
     new_root = etree.Element('gpx', nsmap=nsmap, version='1.1')
 
-    # Process each waypoint
     for wpt in root.findall('wpt'):
         lat = wpt.get('lat')
         lon = wpt.get('lon')
@@ -71,13 +67,11 @@ def convert_gpx(input_file):
 
             extensions = etree.SubElement(new_wpt, 'extensions')
             
-            # Fixed values with the new rule for GP39Symbol
             etree.SubElement(extensions, 'GP39Symbol').text = get_gp39symbol(name)
             etree.SubElement(extensions, 'FECColor').text = str(get_color_code(name))
             etree.SubElement(extensions, 'GP39Comment').text = '1'
             etree.SubElement(extensions, 'GP39Flag').text = '1'
 
-    # Write the new tree to a temporary file
     temp_file = tempfile.NamedTemporaryFile(delete=False, suffix=".gpx")
     new_tree = etree.ElementTree(new_root)
     new_tree.write(temp_file.name, pretty_print=True, xml_declaration=True, encoding='UTF-8')
@@ -98,7 +92,7 @@ def upload_file():
     
     output_file_path = convert_gpx(file)
     
-    return send_file(output_file_path, as_attachment=True, download_name='converted.gpx')
+    return send_file(output_file_path, as_attachment=True, download_name='GP39_Compatible_File.xml')
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
